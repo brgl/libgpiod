7c89d530d6c78865eb9ed0c4ab4c883d558886fc
Bartosz Golaszewski <bartekgola@gmail.com>
2017-01-29 13:06:15 +0100

    libgpiod: relicense under LGPLv2.1
    The library is much less useful under GPLv3 - change the license
    to the more permissive LGPLv2.1 for the entire project.
    
    Signed-off-by: Bartosz Golaszewski <bartekgola@gmail.com>

--- src/tools/gpioinfo.c
+++ src/tools/gpioinfo.c
@@ -3,9 +3,9 @@
  *
  * Copyright (C) 2017 Bartosz Golaszewski <bartekgola@gmail.com>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 3 as
- * published by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2.1 of the GNU Lesser General Public License
+ * as published by the Free Software Foundation.
  */
 
 #include <gpiod.h>
