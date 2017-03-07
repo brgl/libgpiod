c4f76a12706c282f1cd3ca43f1aa3436102447fe
Thierry Reding <treding@nvidia.com>
2017-03-07 14:52:47 +0100

    build: use a more standard autogen.sh
    This version of the autogen.sh script allows execution from a sub-
    directory and will automatically run the configure script that it has
    generated. This is useful for out-of-tree and cross builds.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-autoreconf --force --install --verbose
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+ORIGDIR=`pwd`
+cd "$srcdir"
+
+autoreconf --force --install --verbose || exit 1
+cd $ORIGDIR || exit $?
+
+if test -z "$NOCONFIGURE"; then
+	exec "$srcdir"/configure "$@"
+fi
