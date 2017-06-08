190555c3bc86989e785fce6c21af6f3f6b2fa572
Bartosz Golaszewski <bartekgola@gmail.com>
2017-06-08 15:26:24 +0200

    tests: improve the version string test
    Use the recently introduced regex matching function to verify that
    the version string has a correct format.
    
    Signed-off-by: Bartosz Golaszewski <bartekgola@gmail.com>

--- tests/tests-misc.c
+++ tests/tests-misc.c
@@ -17,6 +17,8 @@ static void version_string(void)
 	/* Check that gpiod_version_string() returns an actual string. */
 	TEST_ASSERT_NOT_NULL(gpiod_version_string());
 	TEST_ASSERT(strlen(gpiod_version_string()) > 0);
+	TEST_ASSERT_REGEX_MATCH(gpiod_version_string(),
+				"^[0-9]+\\.[0-9]+[0-9a-zA-Z\\.]*$");
 }
 TEST_DEFINE(version_string,
 	    "gpiod_version_string()",
