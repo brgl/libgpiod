4fcb25ca3bf2fb8696ef935752a97aaeeaab2b10
Bartosz Golaszewski <bartekgola@gmail.com>
2018-03-04 12:12:56 +0100

    build: user SPDX license identifier
    Use the SPDX license identifier for LGPLv2.1 or later in the remaining
    build and helper files.
    
    Signed-off-by: Bartosz Golaszewski <bartekgola@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+#
+# This file is part of libgpiod.
+#
+# Copyright (C) 2017-2018 Bartosz Golaszewski <bartekgola@gmail.com>
+#                    2017 Thierry Reding <treding@nvidia.com>
+#
 
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
