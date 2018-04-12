brgl/libgpiod

commit 0704bcdbbf560c7f524de02a2163fa03075f30a8

all: update the copyright notice
All these files have been updated in 2018.

Signed-off-by: Bartosz Golaszewski <bartekgola@gmail.com>

src/tools/tools-common.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of libgpiod.
  *
- * Copyright (C) 2017 Bartosz Golaszewski <bartekgola@gmail.com>
+ * Copyright (C) 2017-2018-2018 Bartosz Golaszewski <bartekgola@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
@@ -58,7 +58,7 @@ void print_version(void)
 {
 	printf("%s (libgpiod) %s\n",
 	       program_invocation_short_name, gpiod_version_string());
-	printf("Copyright (C) 2017 Bartosz Golaszewski\n");
+	printf("Copyright (C) 2017-2018-2018 Bartosz Golaszewski\n");
 	printf("License: LGPLv2.1\n");
 	printf("This is free software: you are free to change and redistribute it.\n");
 	printf("There is NO WARRANTY, to the extent permitted by law.\n");

Doxyfile
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 Bartosz Golaszewski <bartekgola@gmail.com>
+# Copyright (C) 2017-2018 Bartosz Golaszewski <bartekgola@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License as published by

Makefile.am
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 Bartosz Golaszewski <bartekgola@gmail.com>
+# Copyright (C) 2017-2018 Bartosz Golaszewski <bartekgola@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License as published by

configure.ac
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 Bartosz Golaszewski <bartekgola@gmail.com>
+# Copyright (C) 2017-2018 Bartosz Golaszewski <bartekgola@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License as published by

include/Makefile.am
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 Bartosz Golaszewski <bartekgola@gmail.com>
+# Copyright (C) 2017-2018 Bartosz Golaszewski <bartekgola@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License as published by

include/gpiod.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of libgpiod.
  *
- * Copyright (C) 2017 Bartosz Golaszewski <bartekgola@gmail.com>
+ * Copyright (C) 2017-2018 Bartosz Golaszewski <bartekgola@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by

src/Makefile.am
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 Bartosz Golaszewski <bartekgola@gmail.com>
+# Copyright (C) 2017-2018 Bartosz Golaszewski <bartekgola@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License as published by

src/lib/Makefile.am
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 Bartosz Golaszewski <bartekgola@gmail.com>
+# Copyright (C) 2017-2018 Bartosz Golaszewski <bartekgola@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License as published by

commit a178da8c95f2fda20278763e9bfea679718eec3e

tools: fix the copyright notice
The copyright notice displayed together with the package version using
the --version argument got accidentally modified by commit 0704bcdbbf56
("all: update the copyright notice") and is now incorrect. Fix it.

Fixes: 0704bcdbbf56 ("all: update the copyright notice")
Signed-off-by: Bartosz Golaszewski <bartekgola@gmail.com>

src/tools/tools-common.c
@@ -52,7 +52,7 @@ void print_version(void)
 {
 	printf("%s (libgpiod) %s\n",
 	       program_invocation_short_name, gpiod_version_string());
-	printf("Copyright (C) 2017-2018-2018 Bartosz Golaszewski\n");
+	printf("Copyright (C) 2017-2018 Bartosz Golaszewski\n");
 	printf("License: LGPLv2.1\n");
 	printf("This is free software: you are free to change and redistribute it.\n");
 	printf("There is NO WARRANTY, to the extent permitted by law.\n");
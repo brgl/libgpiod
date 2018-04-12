a178da8c95f2fda20278763e9bfea679718eec3e
Bartosz Golaszewski <bartekgola@gmail.com>
2018-04-12 17:15:44 +0200

    tools: fix the copyright notice
    The copyright notice displayed together with the package version using
    the --version argument got accidentally modified by commit 0704bcdbbf56
    ("all: update the copyright notice") and is now incorrect. Fix it.
    
    Fixes: 0704bcdbbf56 ("all: update the copyright notice")
    Signed-off-by: Bartosz Golaszewski <bartekgola@gmail.com>

--- src/tools/tools-common.c
+++ src/tools/tools-common.c
@@ -52,7 +52,7 @@ void print_version(void)
 {
 	printf("%s (libgpiod) %s\n",
 	       program_invocation_short_name, gpiod_version_string());
-	printf("Copyright (C) 2017-2018-2018 Bartosz Golaszewski\n");
+	printf("Copyright (C) 2017-2018 Bartosz Golaszewski\n");
 	printf("License: LGPLv2.1\n");
 	printf("This is free software: you are free to change and redistribute it.\n");
 	printf("There is NO WARRANTY, to the extent permitted by law.\n");
