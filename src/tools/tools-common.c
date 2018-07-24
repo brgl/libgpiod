8425c80e7fa80810be298020a7ef1edce9a340c5
Bartosz Golaszewski <bartekgola@gmail.com>
2018-07-24 15:09:35 +0200

    tools: tweak the displayed version format
    Prepend the version string with a 'v' to stay consistent with git
    tags we're using.
    
    Signed-off-by: Bartosz Golaszewski <bartekgola@gmail.com>

--- src/tools/tools-common.c
+++ src/tools/tools-common.c
@@ -50,7 +50,7 @@ void die_perror(const char *fmt, ...)
 
 void print_version(void)
 {
-	printf("%s (libgpiod) %s\n",
+	printf("%s (libgpiod) v%s\n",
 	       program_invocation_short_name, gpiod_version_string());
 	printf("Copyright (C) 2017-2018 Bartosz Golaszewski\n");
 	printf("License: LGPLv2.1\n");
