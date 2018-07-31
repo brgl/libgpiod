brgl/libgpiod

commit 96c524c4951c6ae8d016b7d81ec413cd465333b1

bindings: implement python bindings
Create a C extension module wrapping the functionality of libgpiod
in a set of Python classes.

Add code examples including simplified reimplementations of gpio-tools
in Python.

Signed-off-by: Bartosz Golaszewski <bartekgola@gmail.com>

bindings/python/examples/gpiod_tests.py
@@ -1,0 +1,145 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+#
+# This file is part of libgpiod.
+#
+# Copyright (C) 2017-2018 Bartosz Golaszewski <bartekgola@gmail.com>
+#
+
+'''Misc tests of libgpiod python bindings.'''
+
+import gpiod
+import sys
+
+test_cases = []
+
+def add_test(name, func):
+    global test_cases
+
+    test_cases.append((name, func))
+
+def chip_open():
+    chip = gpiod.Chip('gpiochip0')
+    chip = gpiod.Chip('/dev/gpiochip0', gpiod.Chip.OPEN_BY_PATH)
+    chip = gpiod.Chip('gpiochip0', gpiod.Chip.OPEN_BY_NAME)
+    chip = gpiod.Chip('gpio-mockup-A', gpiod.Chip.OPEN_BY_LABEL)
+    chip = gpiod.Chip('0', gpiod.Chip.OPEN_BY_NUMBER)
+    chip = gpiod.Chip('gpio-mockup-A', gpiod.Chip.OPEN_LOOKUP)
+    print('All good')
+
+add_test('Open a GPIO chip using different modes', chip_open)
+
+def chip_open_no_args():
+    try:
+        chip = gpiod.Chip()
+    except TypeError:
+        print('Error as expected')
+        return
+
+    assert False, 'TypeError expected'
+
+add_test('Open a GPIO chip without arguments', chip_open_no_args)
+
+def chip_info():
+    chip = gpiod.Chip('gpiochip0')
+    print('name: {}'.format(chip.name()))
+    print('label: {}'.format(chip.label()))
+    print('lines: {}'.format(chip.num_lines()))
+
+add_test('Print chip info', chip_info)
+
+def print_chip():
+    chip = gpiod.Chip('/dev/gpiochip0')
+    print(chip)
+
+add_test('Print chip object', print_chip)
+
+def create_line_object():
+    try:
+        line = gpiod.Line()
+    except NotImplementedError:
+        print('Error as expected')
+        return
+
+    assert False, 'NotImplementedError expected'
+
+add_test('Create a line object - should fail', create_line_object)
+
+def print_line():
+    chip = gpiod.Chip('gpio-mockup-A')
+    line = chip.get_line(3)
+    print(line)
+
+add_test('Print line object', print_line)
+
+def find_line():
+    line = gpiod.find_line('gpio-mockup-A-4')
+    print('found line - offset: {}'.format(line.offset()))
+
+add_test('Find line globally', find_line)
+
+def create_empty_line_bulk():
+    try:
+        lines = gpiod.LineBulk()
+    except TypeError:
+        print('Error as expected')
+        return
+
+    assert False, 'TypeError expected'
+
+add_test('Create a line bulk object - should fail', create_empty_line_bulk)
+
+def get_lines():
+    chip = gpiod.Chip('gpio-mockup-A')
+    lines = chip.get_lines([2, 4, 5, 7])
+    print('Retrieved lines:')
+    for line in lines:
+        print(line)
+
+add_test('Get lines from chip', get_lines)
+
+def create_line_bulk_from_lines():
+    chip = gpiod.Chip('gpio-mockup-A')
+    line1 = chip.get_line(2)
+    line2 = chip.get_line(4)
+    line3 = chip.get_line(6)
+    lines = gpiod.LineBulk([line1, line2, line3])
+    print('Created LineBulk:')
+    print(lines)
+
+add_test('Create a LineBulk from a list of lines', create_line_bulk_from_lines)
+
+def line_bulk_to_list():
+    chip = gpiod.Chip('gpio-mockup-A')
+    lines = chip.get_lines((1, 2, 3))
+    print(lines.to_list())
+
+add_test('Convert a LineBulk to a list', line_bulk_to_list)
+
+def get_value_single_line():
+    chip = gpiod.Chip('gpio-mockup-A')
+    line = chip.get_line(2)
+    line.request(consumer=sys.argv[0], type=gpiod.LINE_REQ_DIR_IN)
+    print('line value: {}'.format(line.get_value()))
+
+add_test('Get value - single line', get_value_single_line)
+
+def set_value_single_line():
+    chip = gpiod.Chip('gpiochip0')
+    line = chip.get_line(3)
+    line.request(consumer=sys.argv[0], type=gpiod.LINE_REQ_DIR_IN)
+    print('line value before: {}'.format(line.get_value()))
+    line.release()
+    line.request(consumer=sys.argv[0], type=gpiod.LINE_REQ_DIR_OUT)
+    line.set_value(1)
+    line.release()
+    line.request(consumer=sys.argv[0], type=gpiod.LINE_REQ_DIR_IN)
+    print('line value after: {}'.format(line.get_value()))
+
+add_test('Set value - single line', set_value_single_line)
+
+for name, func in test_cases:
+    print('==============================================')
+    print('{}:'.format(name))
+    func()

.gitignore
@@ -17,6 +17,7 @@ gpiomoncxx
 gpiosetcxx
 *.o
 *.lo
+*.la
 doc
 libgpiod.pc
 libgpiodcxx.pc

bindings/python/Makefile.am
@@ -1,0 +1,18 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+#
+# This file is part of libgpiod.
+#
+# Copyright (C) 2017-2018 Bartosz Golaszewski <bartekgola@gmail.com>
+#
+
+SUBDIRS = . examples
+
+pyexec_LTLIBRARIES = gpiod.la
+
+gpiod_la_SOURCES = gpiodmodule.c
+
+gpiod_la_CFLAGS = -I$(top_srcdir)/include/ -include $(top_builddir)/config.h
+gpiod_la_CFLAGS += $(PYTHON_CPPFLAGS) -Wall -Wextra -g
+gpiod_la_LDFLAGS = -module -avoid-version
+gpiod_la_LIBADD = $(top_builddir)/src/lib/libgpiod.la

bindings/python/examples/Makefile.am
@@ -6,16 +6,10 @@
 # Copyright (C) 2017-2018 Bartosz Golaszewski <bartekgola@gmail.com>
 #
 
-SUBDIRS = .
-
-if WITH_BINDINGS_CXX
-
-SUBDIRS += cxx
-
-endif
-
-if WITH_BINDINGS_PYTHON
-
-SUBDIRS += python
-
-endif
+EXTRA_DIST =	gpiod_tests.py \
+		gpiodetect.py \
+		gpiofind.py \
+		gpioget.py \
+		gpioinfo.py \
+		gpiomon.py \
+		gpioset.py

bindings/python/examples/gpiodetect.py
@@ -1,0 +1,17 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+#
+# This file is part of libgpiod.
+#
+# Copyright (C) 2017-2018 Bartosz Golaszewski <bartekgola@gmail.com>
+#
+
+'''Reimplementation of the gpiodetect tool in Python.'''
+
+import gpiod
+
+for chip in gpiod.ChipIter():
+    print('{} [{}] ({} lines)'.format(chip.name(),
+                                      chip.label(),
+                                      chip.num_lines()))

bindings/python/examples/gpiofind.py
@@ -1,0 +1,16 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+#
+# This file is part of libgpiod.
+#
+# Copyright (C) 2017-2018 Bartosz Golaszewski <bartekgola@gmail.com>
+#
+
+'''Reimplementation of the gpiofind tool in Python.'''
+
+import gpiod
+import sys
+
+line = gpiod.find_line(sys.argv[1])
+print('{} {}'.format(line.owner().name(), line.offset()))

bindings/python/examples/gpioget.py
@@ -1,0 +1,30 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+#
+# This file is part of libgpiod.
+#
+# Copyright (C) 2017-2018 Bartosz Golaszewski <bartekgola@gmail.com>
+#
+
+'''Simplified reimplementation of the gpioget tool in Python.'''
+
+import gpiod
+import sys
+
+if len(sys.argv) < 3:
+    raise TypeError('usage: gpioget.py <gpiochip> <offset1> <offset2> ...')
+
+chip = gpiod.Chip(sys.argv[1])
+
+offsets = []
+for off in sys.argv[2:]:
+    offsets.append(int(off))
+
+lines = chip.get_lines(offsets)
+lines.request(consumer=sys.argv[0], type=gpiod.LINE_REQ_DIR_IN)
+vals = lines.get_values()
+
+for val in vals:
+    print(val, end=' ')
+print()

commit cdf5ef94c454b8ebab432cc9172250379545ec8a

bindings: python: fix gpiod_Chip_find_lines() for nonexistent lines
Currently if we call gpiod_Chip_find_lines() with some names that
cannot be looked up, we end up getting an exception from the LineBulk
object's constructor (because None is not a Line and LineBulk can only
hold Line objects).

Fix the behavior of gpiod_Chip_find_lines() for nonexistent lines by
raising an explicit exception if the underlying gpiod_Chip_find_line()
call returns None. Also: add a relevant test case.

Fixes: 96c524c4951c ("bindings: implement python bindings")
Signed-off-by: Bartosz Golaszewski <bartekgola@gmail.com>

bindings/python/gpiodmodule.c
@@ -1646,9 +1646,12 @@ gpiod_Chip_find_lines(gpiod_ChipObject *self, PyObject *args)
 
 		line = gpiod_Chip_find_line(self, arg);
 		Py_DECREF(arg);
-		if (!line) {
+		if (!line || (PyObject *)line == Py_None) {
 			Py_DECREF(iter);
 			Py_DECREF(lines);
+			if ((PyObject *)line == Py_None)
+				PyErr_SetString(PyExc_TypeError,
+						"Unable to find all lines from the list");
 			return NULL;
 		}
 

bindings/python/examples/gpiod_tests.py
@@ -215,6 +215,20 @@ def find_lines():
 
 add_test('Find multiple lines by name', find_lines)
 
+def find_lines_one_bad():
+    chip = gpiod.Chip('gpiochip0')
+
+    print('looking up lines by names')
+    try:
+        lines = chip.find_lines(['gpio-mockup-A-3', 'nonexistent', 'gpio-mockup-A-7'])
+    except TypeError as ex:
+        print('Error as expected')
+        return
+
+    assert False, 'TypeError expected'
+
+add_test('Find multiple lines but one line name is non-existent', find_lines_one_bad)
+
 def create_line_bulk_from_lines():
     chip = gpiod.Chip('gpio-mockup-A')
     line1 = chip.get_line(2)