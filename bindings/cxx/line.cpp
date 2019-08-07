brgl/libgpiod

commit 8078a4a2ad90caf95ef6426c2b5cddeeddc9dc57

bindings: implement C++ bindings
Wrap the C API in a set of C++ classes which allow to use all libgpiod
functionalities in an object-oriented manner.

Include simplified reimplementations of gpio-tools and a set of
tests/examples showing how to use the C++ interface.

The C++ API is fully documented in doxygen.

Signed-off-by: Bartosz Golaszewski <bartekgola@gmail.com>

bindings/cxx/line.cpp
@@ -1,0 +1,238 @@
+/*
+ * This file is part of libgpiod.
+ *
+ * Copyright (C) 2017-2018 Bartosz Golaszewski <bartekgola@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ */
+
+#include <gpiod.hpp>
+
+#include <system_error>
+
+namespace gpiod {
+
+line::line(void)
+	: _m_line(nullptr),
+	  _m_chip()
+{
+
+}
+
+line::line(::gpiod_line* line, const chip& owner)
+	: _m_line(line),
+	  _m_chip(owner)
+{
+
+}
+
+unsigned int line::offset(void) const
+{
+	this->throw_if_null();
+
+	return ::gpiod_line_offset(this->_m_line);
+}
+
+::std::string line::name(void) const
+{
+	this->throw_if_null();
+
+	const char* name = ::gpiod_line_name(this->_m_line);
+
+	return ::std::move(name ? ::std::string(name) : ::std::string());
+}
+
+::std::string line::consumer(void) const
+{
+	this->throw_if_null();
+
+	const char* consumer = ::gpiod_line_consumer(this->_m_line);
+
+	return ::std::move(consumer ? ::std::string(consumer) : ::std::string());
+}
+
+int line::direction(void) const noexcept
+{
+	this->throw_if_null();
+
+	int dir = ::gpiod_line_direction(this->_m_line);
+
+	return dir == GPIOD_LINE_DIRECTION_INPUT ? DIRECTION_INPUT : DIRECTION_OUTPUT;
+}
+
+int line::active_state(void) const noexcept
+{
+	this->throw_if_null();
+
+	int active = ::gpiod_line_active_state(this->_m_line);
+
+	return active == GPIOD_LINE_ACTIVE_STATE_HIGH ? ACTIVE_HIGH : ACTIVE_LOW;
+}
+
+bool line::is_used(void) const
+{
+	this->throw_if_null();
+
+	return ::gpiod_line_is_used(this->_m_line);
+}
+
+bool line::is_open_drain(void) const
+{
+	this->throw_if_null();
+
+	return ::gpiod_line_is_open_drain(this->_m_line);
+}
+
+bool line::is_open_source(void) const
+{
+	this->throw_if_null();
+
+	return ::gpiod_line_is_open_source(this->_m_line);
+}
+
+void line::request(const line_request& config, int default_val) const
+{
+	this->throw_if_null();
+
+	line_bulk bulk({ *this });
+
+	bulk.request(config, { default_val });
+}
+
+bool line::is_requested(void) const
+{
+	this->throw_if_null();
+
+	return ::gpiod_line_is_requested(this->_m_line);
+}
+
+/*
+ * REVISIT: Check the performance of get/set_value & event_wait compared to
+ * the C API. Creating a line_bulk object involves a memory allocation every
+ * time this method if called. If the performance is significantly lower,
+ * switch to calling the C functions for setting/getting line values and
+ * polling for events on single lines directly.
+ */
+
+int line::get_value(void) const
+{
+	this->throw_if_null();
+
+	line_bulk bulk({ *this });
+
+	return bulk.get_values()[0];
+}
+
+void line::set_value(int val) const
+{
+	this->throw_if_null();
+
+	line_bulk bulk({ *this });
+
+	bulk.set_values({ val });
+}
+
+bool line::event_wait(const ::std::chrono::nanoseconds& timeout) const
+{
+	this->throw_if_null();
+
+	line_bulk bulk({ *this });
+
+	line_bulk event_bulk = bulk.event_wait(timeout);
+
+	return ::std::move(event_bulk);
+}
+
+line_event line::event_read(void) const
+{
+	this->throw_if_null();
+
+	::gpiod_line_event event_buf;
+	line_event event;
+	int rv;
+
+	rv = ::gpiod_line_event_read(this->_m_line, ::std::addressof(event_buf));
+	if (rv < 0)
+		throw ::std::system_error(errno, ::std::system_category(),
+					  "error reading line event");
+
+	if (event_buf.event_type == GPIOD_LINE_EVENT_RISING_EDGE)
+		event.event_type = line_event::RISING_EDGE;
+	else if (event_buf.event_type == GPIOD_LINE_EVENT_FALLING_EDGE)
+		event.event_type = line_event::FALLING_EDGE;
+
+	event.timestamp = ::std::chrono::nanoseconds(
+				event_buf.ts.tv_nsec + (event_buf.ts.tv_sec * 1000000000));
+
+	event.source = *this;
+
+	return ::std::move(event);
+}
+
+int line::event_get_fd(void) const
+{
+	this->throw_if_null();
+
+	int ret = ::gpiod_line_event_get_fd(this->_m_line);
+
+	if (ret < 0)
+		::std::system_error(errno, ::std::system_category(),
+				    "unable to get the line event file descriptor");
+
+	return ret;
+}
+
+const chip& line::get_chip(void) const
+{
+	return this->_m_chip;
+}
+
+void line::reset(void)
+{
+	this->_m_line = nullptr;
+	this->_m_chip.reset();
+}
+
+bool line::operator==(const line& rhs) const noexcept
+{
+	return this->_m_line == rhs._m_line;
+}
+
+bool line::operator!=(const line& rhs) const noexcept
+{
+	return this->_m_line != rhs._m_line;
+}
+
+line::operator bool(void) const noexcept
+{
+	return this->_m_line != nullptr;
+}
+
+bool line::operator!(void) const noexcept
+{
+	return this->_m_line == nullptr;
+}
+
+void line::throw_if_null(void) const
+{
+	if (!this->_m_line)
+		throw ::std::logic_error("object not holding a GPIO line handle");
+}
+
+line find_line(const ::std::string& name)
+{
+	line ret;
+
+	for (auto& it: make_chip_iterator()) {
+		ret = it.find_line(name);
+		if (ret)
+			break;
+	}
+
+	return ::std::move(ret);
+}
+
+} /* namespace gpiod */

.gitignore
@@ -1,5 +1,6 @@
 !.gitignore
 libgpiod.la
+libgpiodcxx.la
 libtools-common.la
 gpiodetect
 gpioinfo
@@ -7,6 +8,13 @@ gpioget
 gpioset
 gpiomon
 gpiofind
+gpio_cxx_tests
+gpiodetectcxx
+gpiofindcxx
+gpiogetcxx
+gpioinfocxx
+gpiomoncxx
+gpiosetcxx
 *.o
 *.lo
 doc

Makefile.am
@@ -9,7 +9,7 @@
 
 ACLOCAL_AMFLAGS = -I m4
 AUTOMAKE_OPTIONS = foreign
-SUBDIRS = include src
+SUBDIRS = include src bindings
 
 pkgconfigdir = $(libdir)/pkgconfig
 pkgconfig_DATA = libgpiod.pc
@@ -23,7 +23,9 @@ endif
 if HAS_DOXYGEN
 
 doc:
-	@(cat Doxyfile; echo PROJECT_NUMBER = $(VERSION_STR)) | doxygen -
+	@(cat Doxyfile; \
+		echo PROJECT_NUMBER = $(VERSION_STR); \
+		echo INPUT += bindings/cxx/gpiod.hpp) | doxygen -
 .PHONY: doc
 
 EXTRA_DIST = Doxyfile

bindings/Makefile.am
@@ -1,0 +1,16 @@
+#
+# Copyright (C) 2017-2018 Bartosz Golaszewski <bartekgola@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or (at
+# your option) any later version.
+#
+
+SUBDIRS = .
+
+if WITH_BINDINGS_CXX
+
+SUBDIRS += cxx
+
+endif

bindings/cxx/Makefile.am
@@ -1,0 +1,19 @@
+#
+# Copyright (C) 2017-2018 Bartosz Golaszewski <bartekgola@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or (at
+# your option) any later version.
+#
+
+lib_LTLIBRARIES = libgpiodcxx.la
+libgpiodcxx_la_SOURCES = chip.cpp iter.cpp line.cpp line_bulk.cpp
+libgpiodcxx_la_CPPFLAGS = -Wall -Wextra -g -std=gnu++11
+libgpiodcxx_la_CPPFLAGS += -fvisibility=hidden -I$(top_srcdir)/include/
+libgpiodcxx_la_LDFLAGS = -version-number $(subst .,:,$(PACKAGE_VERSION))
+libgpiodcxx_la_LDFLAGS += -lgpiod -L$(top_builddir)/src/lib
+
+include_HEADERS = gpiod.hpp
+
+SUBDIRS = . examples

bindings/cxx/chip.cpp
@@ -1,0 +1,192 @@
+/*
+ * This file is part of libgpiod.
+ *
+ * Copyright (C) 2017-2018 Bartosz Golaszewski <bartekgola@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ */
+
+#include <gpiod.hpp>
+
+#include <system_error>
+#include <functional>
+#include <utility>
+#include <map>
+
+namespace gpiod {
+
+namespace {
+
+::gpiod_chip* open_lookup(const ::std::string& device)
+{
+	return ::gpiod_chip_open_lookup(device.c_str());
+}
+
+::gpiod_chip* open_by_path(const ::std::string& device)
+{
+	return ::gpiod_chip_open(device.c_str());
+}
+
+::gpiod_chip* open_by_name(const ::std::string& device)
+{
+	return ::gpiod_chip_open_by_name(device.c_str());
+}
+
+::gpiod_chip* open_by_label(const ::std::string& device)
+{
+	return ::gpiod_chip_open_by_label(device.c_str());
+}
+
+::gpiod_chip* open_by_number(const ::std::string& device)
+{
+	return ::gpiod_chip_open_by_number(::std::stoul(device));
+}
+
+using open_func = ::std::function<::gpiod_chip* (const ::std::string&)>;
+
+const ::std::map<int, open_func> open_funcs = {
+	{ chip::OPEN_LOOKUP,	open_lookup,	},
+	{ chip::OPEN_BY_PATH,	open_by_path,	},
+	{ chip::OPEN_BY_NAME,	open_by_name,	},
+	{ chip::OPEN_BY_LABEL,	open_by_label,	},
+	{ chip::OPEN_BY_NUMBER,	open_by_number,	},
+};
+
+void chip_deleter(::gpiod_chip* chip)
+{
+	::gpiod_chip_close(chip);
+}
+
+} /* namespace */
+
+chip::chip(const ::std::string& device, int how)
+	: _m_chip()
+{
+	this->open(device, how);
+}
+
+chip::chip(::gpiod_chip* chip)
+	: _m_chip(chip, chip_deleter)
+{
+
+}
+
+void chip::open(const ::std::string& device, int how)
+{
+	auto func = open_funcs.at(how);
+
+	::gpiod_chip *chip = func(device);
+	if (!chip)
+		throw ::std::system_error(errno, ::std::system_category(),
+					  "cannot open GPIO device " + device);
+
+	this->_m_chip.reset(chip, chip_deleter);
+}
+
+void chip::reset(void) noexcept
+{
+	this->_m_chip.reset();
+}
+
+::std::string chip::name(void) const
+{
+	this->throw_if_noref();
+
+	const char* name = ::gpiod_chip_name(this->_m_chip.get());
+
+	return ::std::move(name ? ::std::string(name) : ::std::string());
+}
+
+::std::string chip::label(void) const
+{
+	this->throw_if_noref();
+
+	const char* label = ::gpiod_chip_label(this->_m_chip.get());
+
+	return ::std::move(label ? ::std::string(label) : ::std::string());
+}
+
+unsigned int chip::num_lines(void) const
+{
+	this->throw_if_noref();
+
+	return ::gpiod_chip_num_lines(this->_m_chip.get());
+}
+
+line chip::get_line(unsigned int offset) const
+{
+	this->throw_if_noref();
+
+	if (offset >= this->num_lines())
+		throw ::std::out_of_range("line offset greater than the number of lines");
+
+	::gpiod_line* line_handle = ::gpiod_chip_get_line(this->_m_chip.get(), offset);
+	if (!line_handle)
+		throw ::std::system_error(errno, ::std::system_category(),
+					  "error getting GPIO line from chip");
+
+	return ::std::move(line(line_handle, *this));
+}
+
+line chip::find_line(const ::std::string& name) const
+{
+	this->throw_if_noref();
+
+	::gpiod_line* handle = ::gpiod_chip_find_line(this->_m_chip.get(), name.c_str());
+	if (!handle && errno != ENOENT)
+		throw ::std::system_error(errno, ::std::system_category(),
+					  "error looking up GPIO line by name");
+
+	return ::std::move(handle ? line(handle, *this) : line());
+}
+
+line_bulk chip::get_lines(const ::std::vector<unsigned int>& offsets) const
+{
+	line_bulk lines;
+
+	for (auto& it: offsets)
+		lines.add(this->get_line(it));
+
+	return ::std::move(lines);
+}
+
+line_bulk chip::find_lines(const ::std::vector<::std::string>& names) const
+{
+	line_bulk lines;
+
+	for (auto& it: names)
+		lines.add(this->find_line(it));
+
+	return ::std::move(lines);
+}
+
+bool chip::operator==(const chip& rhs) const noexcept
+{
+	return this->_m_chip.get() == rhs._m_chip.get();
+}
+
+bool chip::operator!=(const chip& rhs) const noexcept
+{
+	return this->_m_chip.get() != rhs._m_chip.get();
+}
+
+chip::operator bool(void) const noexcept
+{
+	return this->_m_chip.get() != nullptr;
+}
+
+bool chip::operator!(void) const noexcept
+{
+	return this->_m_chip.get() == nullptr;
+}
+
+void chip::throw_if_noref(void) const
+{
+	if (!this->_m_chip.get())
+		throw ::std::logic_error("object not associated with an open GPIO chip");
+}
+
+} /* namespace gpiod */

bindings/cxx/examples/Makefile.am
@@ -1,0 +1,34 @@
+#
+# Copyright (C) 2017-2018 Bartosz Golaszewski <bartekgola@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or (at
+# your option) any later version.
+#
+
+AM_CPPFLAGS = -I$(top_srcdir)/bindings/cxx/ -I$(top_srcdir)/include
+AM_CPPFLAGS += -Wall -Wextra -g -std=gnu++11
+AM_LDFLAGS = -lgpiodcxx -L$(top_builddir)/bindings/cxx/
+
+check_PROGRAMS =	gpio_cxx_tests \
+			gpiodetectcxx \
+			gpiofindcxx \
+			gpiogetcxx \
+			gpioinfocxx \
+			gpiomoncxx \
+			gpiosetcxx
+
+gpio_cxx_tests_SOURCES = gpio_cxx_tests.cpp
+
+gpiodetectcxx_SOURCES = gpiodetectcxx.cpp
+
+gpiofindcxx_SOURCES = gpiofindcxx.cpp
+
+gpiogetcxx_SOURCES = gpiogetcxx.cpp
+
+gpioinfocxx_SOURCES = gpioinfocxx.cpp
+
+gpiomoncxx_SOURCES = gpiomoncxx.cpp
+
+gpiosetcxx_SOURCES = gpiosetcxx.cpp

bindings/cxx/examples/gpio_cxx_tests.cpp
@@ -1,0 +1,161 @@
+/*
+ * This file is part of libgpiod.
+ *
+ * Copyright (C) 2017-2018 Bartosz Golaszewski <bartekgola@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ */
+
+/* Misc tests/examples of the C++ API. */
+
+#include <gpiod.hpp>
+
+#include <stdexcept>
+#include <cstdlib>
+#include <iostream>
+
+namespace {
+
+using test_func = ::std::function<void (void)>;
+
+::std::vector<::std::pair<::std::string, test_func>> test_funcs;
+
+struct test_case
+{
+	test_case(const ::std::string& name, test_func& func)
+	{
+		test_funcs.push_back(::std::make_pair(name, func));
+	}
+};
+
+#define TEST_CASE(_func)						\
+	test_func _test_func_##_func(_func);				\
+	test_case _test_case_##_func(#_func, _test_func_##_func)
+
+void chip_info(void)
+{
+	::gpiod::chip chip("gpiochip0");
+
+	::std::cout << "chip name: " << chip.name() << ::std::endl;
+	::std::cout << "chip label: " << chip.label() << ::std::endl;
+	::std::cout << "number of lines " << chip.num_lines() << ::std::endl;
+}
+TEST_CASE(chip_info);
+
+void chip_open_different_modes(void)
+{
+	::gpiod::chip by_name("gpiochip0", ::gpiod::chip::OPEN_BY_NAME);
+	::gpiod::chip by_path("/dev/gpiochip0", ::gpiod::chip::OPEN_BY_PATH);
+	::gpiod::chip by_label("gpio-mockup-A", ::gpiod::chip::OPEN_BY_LABEL);
+	::gpiod::chip by_number("0", ::gpiod::chip::OPEN_BY_NUMBER);
+}
+TEST_CASE(chip_open_different_modes);
+
+void chip_line_ops(void)
+{
+	::gpiod::chip chip("gpiochip0");
+
+	::gpiod::line line = chip.get_line(3);
+	::std::cout << "Got line by offset: " << line.offset() << ::std::endl;
+
+	line = chip.find_line("gpio-mockup-A-4");
+	::std::cout << "Got line by name: " << line.name() << ::std::endl;
+
+	::gpiod::line_bulk lines = chip.get_lines({ 1, 2, 3, 6, 6 });
+	::std::cout << "Got multiple lines by offset: ";
+	for (auto& it: lines)
+		::std::cout << it.offset() << " ";
+	::std::cout << ::std::endl;
+
+	lines = chip.find_lines({ "gpio-mockup-A-1", "gpio-mockup-A-4", "gpio-mockup-A-7" });
+	::std::cout << "Got multiple lines by name: ";
+	for (auto& it: lines)
+		::std::cout << it.name() << " ";
+	::std::cout << ::std::endl;
+}
+TEST_CASE(chip_line_ops);
+
+void line_info(void)
+{
+	::gpiod::chip chip("gpiochip0");
+	::gpiod::line line = chip.get_line(2);
+
+	::std::cout << "line offset: " << line.offset() << ::std::endl;
+	::std::cout << "line name: " << line.name() << ::std::endl;
+	::std::cout << "line direction: "
+		    << (line.direction() == ::gpiod::line::DIRECTION_INPUT ?
+			"input" : "output") << ::std::endl;
+}
+TEST_CASE(line_info);
+
+void empty_objects(void)
+{
+	::std::cout << "Are initialized line & chip objects 'false'?" << ::std::endl;
+
+	::gpiod::line line;
+	if (line)
+		throw ::std::logic_error("line built with a default constructor should be 'false'");
+
+	::gpiod::chip chip;
+	if (chip)
+		throw ::std::logic_error("chip built with a default constructor should be 'false'");
+
+	::std::cout << "YES" << ::std::endl;
+}
+TEST_CASE(empty_objects);
+
+void line_bulk_iterator(void)
+{
+	::std::cout << "Checking line_bulk iterators" << ::std::endl;
+
+	::gpiod::chip chip("gpiochip0");
+	::gpiod::line_bulk bulk = chip.get_lines({ 0, 1, 2, 3, 4 });
+
+	for (auto& it: bulk)
+		::std::cout << it.name() << ::std::endl;
+
+	::std::cout << "DONE" << ::std::endl;
+}
+TEST_CASE(line_bulk_iterator);
+
+void single_line_test(void)
+{
+	const ::std::string line_name("gpio-mockup-A-4");
+
+	::std::cout << "Looking up a GPIO line by name (" << line_name << ")" << ::std::endl;
+
+	::gpiod::line line = ::gpiod::find_line(line_name);
+	if (!line)
+		throw ::std::runtime_error(line_name + " line not found");
+
+	::std::cout << "Requesting a single line" << ::std::endl;
+
+	::gpiod::line_request conf;
+	conf.consumer = "gpiod_cxx_tests";
+	conf.request_type = ::gpiod::line_request::DIRECTION_OUTPUT;
+
+	line.request(conf, 1);
+	::std::cout << "Reading value" << ::std::endl;
+	::std::cout << line.get_value() << ::std::endl;
+	::std::cout << "Changing value to 0" << ::std::endl;
+	line.set_value(0);
+	::std::cout << "Reading value again" << ::std::endl;
+	::std::cout << line.get_value() << ::std::endl;
+}
+TEST_CASE(single_line_test);
+
+} /* namespace */
+
+int main(int, char **)
+{
+	for (auto& it: test_funcs) {
+		::std::cout << "=================================================" << ::std::endl;
+		::std::cout << it.first << ":\n" << ::std::endl;
+		it.second();
+	}
+
+	return EXIT_SUCCESS;
+}

commit d001dde47cc0df44ad79aa19bfe5bafaf4ba31bd

bindings: cxx: add a missing throw to line::event_get_fd()
If the call to gpiod_line_event_get_fd() fails, we're not actually
throwing the exception - we're only creating it on stack. Add the
missing throw keyword.

Fixes: 8078a4a2ad90 ("bindings: implement C++ bindings")
Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>

bindings/cxx/line.cpp
@@ -183,8 +183,8 @@ int line::event_get_fd(void) const
 	int ret = ::gpiod_line_event_get_fd(this->_m_line);
 
 	if (ret < 0)
-		::std::system_error(errno, ::std::system_category(),
-				    "unable to get the line event file descriptor");
+		throw ::std::system_error(errno, ::std::system_category(),
+					  "unable to get the line event file descriptor");
 
 	return ret;
 }