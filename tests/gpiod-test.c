c0176f58f98e837f1b9eb40514ac58c51f24e644
Bartosz Golaszewski <bartosz.golaszewski@linaro.org>
2022-12-15 19:50:12 +0100

    tests: consistently use GLib types in tests
    We use GLib in tests so instead of int use gint everywhere.
    
    Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@linaro.org>

--- tests/gpiod-test.c
+++ tests/gpiod-test.c
@@ -60,7 +60,7 @@ static void add_test_from_list(gpointer element, gpointer data G_GNUC_UNUSED)
 	g_test_add_data_func(test->path, test, test_func_wrapper);
 }
 
-int main(gint argc, gchar **argv)
+gint main(gint argc, gchar **argv)
 {
 	g_test_init(&argc, &argv, NULL);
 	g_test_set_nonfatal_assertions();
