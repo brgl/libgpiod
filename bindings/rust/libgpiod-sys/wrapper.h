caabf537b54140e6c9682dd940f1fa9ccf2e2dc3
Bartosz Golaszewski <bartosz.golaszewski@linaro.org>
2023-06-13 09:24:42 +0200

    bindings: rust: add missing license and copyright boilerplate
    Make reuse happy again by adding appropriate license and copyright info.
    
    Fixes: 1f8085953086 ("bindings: rust: build against pkg-config info")
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Acked-by: Erik Schilling <erik.schilling@linaro.org>
    Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@linaro.org>

--- bindings/rust/libgpiod-sys/wrapper.h
+++ bindings/rust/libgpiod-sys/wrapper.h
@@ -1,1 +1,5 @@
+/* SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause */
+/* SPDX-FileCopyrightText: 2023 Linaro Ltd. */
+/* SPDX-FileCopyrightText: 2023 Erik Schilling <erik.schilling@linaro.org> */
+
 #include <gpiod.h>
