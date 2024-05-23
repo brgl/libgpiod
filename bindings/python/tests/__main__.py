27fe10150f6d5fa78d1a1ef1e922dc8395d1154d
Bartosz Golaszewski <bartosz.golaszewski@linaro.org>
2024-05-23 10:08:31 +0200

    bindings: python: tests: run the Value cast test with the rest of the suite
    Make the new test file for line definitions part of the entire test-suite
    for python bindings.
    
    Fixes: c8e3ae0499c8 ("bindings: python: tests: add test for casting line.Value to bool")
    Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@linaro.org>

--- bindings/python/tests/__main__.py
+++ bindings/python/tests/__main__.py
@@ -8,6 +8,7 @@ from .tests_chip import *
 from .tests_chip_info import *
 from .tests_edge_event import *
 from .tests_info_event import *
+from .tests_line import *
 from .tests_line_info import *
 from .tests_line_settings import *
 from .tests_module import *
