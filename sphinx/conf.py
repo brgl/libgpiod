824c1f39347c2ef46919dfc45e8247441b908827
Kent Gibson <warthog618@gmail.com>
2024-07-05 10:17:50 +0800

    doc: fix sphinx config for rtd
    Generating the latest documentation on readthedocs is broken as the
    index.html generated by Doxygen is now being overwritten by one
    generated by Sphinx.
    
    Make Sphinx generate a differently named root page that does not
    conflict with the index.html generated by Doxygen.
    
    Signed-off-by: Kent Gibson <warthog618@gmail.com>
    Link: https://lore.kernel.org/r/20240705021750.43197-1-warthog618@gmail.com
    Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@linaro.org>

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -53,6 +53,8 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 
+root_doc = 'contents'
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
