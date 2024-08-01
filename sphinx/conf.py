51fe942219bde0c8224537933a32596f034efebd
Bartosz Golaszewski <bartosz.golaszewski@linaro.org>
2024-08-01 10:01:38 +0200

    sphinx: fix coding style
    Use black to automatically correct the coding style of the sphinx
    config file.
    
    Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@linaro.org>

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -53,7 +53,7 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 
-root_doc = 'contents'
+root_doc = "contents"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
