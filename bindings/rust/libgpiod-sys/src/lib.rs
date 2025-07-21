b20401360ce35fabc123c379ab3fddb394ee467e
Erik Wierich <erik@riscstar.com>
2025-07-21 09:53:07 +0200

    bindings: rust: remove newline between attribute and attribute
    These are block attributes, but the newline in between suggests that
    they are for the whole module.
    
    Fixes clippy warning:
    
        warning: empty line after outer attribute
         --> libgpiod-sys/src/lib.rs:6:1
          |
        6 | / #[cfg_attr(test, allow(deref_nullptr, non_snake_case))]
        7 | |
          | |_^
        8 |   mod bindings_raw {
          |   ---------------- the attribute applies to this module
          |
          = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#empty_line_after_outer_attr
          = note: `#[warn(clippy::empty_line_after_outer_attr)]` on by default
          = help: if the empty line is unintentional, remove it
        help: if the attribute should apply to the crate use an inner attribute
          |
        5 ~ #![allow(non_camel_case_types, non_upper_case_globals)]
        6 ~ #![cfg_attr(test, allow(deref_nullptr, non_snake_case))]
          |
    
    Signed-off-by: Erik Wierich <erik@riscstar.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Link: https://lore.kernel.org/r/20250721-rust-clippy-v1-1-2ac0198b2ea6@riscstar.com
    Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@linaro.org>

--- bindings/rust/libgpiod-sys/src/lib.rs
+++ bindings/rust/libgpiod-sys/src/lib.rs
@@ -4,7 +4,6 @@
 
 #[allow(non_camel_case_types, non_upper_case_globals)]
 #[cfg_attr(test, allow(deref_nullptr, non_snake_case))]
-
 mod bindings_raw {
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
