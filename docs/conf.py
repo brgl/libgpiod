brgl/libgpiod

commit 97bab4cc87ddca0e4425ec384dd4fc1f0b8902a6

build: replace autotools with meson & ninja
Port the project from autotools to meson. All build options are
preserved with the same semantics.

The goal of this change is to produce the same set of files as the
current autotools approach: libraries with correct SONAME/version,
headers, pkg-config files, GObject introspection data, Python extension
modules, D-Bus interface and configuration files. This is done to enable
a one-liner test command running all available test suites.

The only thing we drop is the `make docs` target as it's rarely used and
docs can be generated easily with sphinx directly. They are also not
required for testing.

The Rust workspace is built via a custom target that invokes cargo and
uses SYSTEM_DEPS_* environment variables to point the libgpiod-sys and
gpiosim-sys crates at the locally built C libraries.

In addition to porting the build files to meson, update the docs, remove
unneded items from .gitignore files (or remove them entirely if they're
no longer needed) and update the shunit2 test scripts to no longer care
about source directories.

Tested-by: Erik Wierich <erik@riscstar.com>
Tested-by: Vincent Fazio <vfazio@xes-inc.com>
Link: https://patch.msgid.link/20260428-meson-build-v3-1-8da6976fb0a2@oss.qualcomm.com
Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@oss.qualcomm.com>

bindings/glib/meson.build
@@ -1,0 +1,112 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-FileCopyrightText: 2026 Qualcomm Technologies, Inc. and/or its subsidiaries
+
+subdir('gpiod-glib')
+
+project_headers = files(
+  'gpiod-glib/line.h',
+  'gpiod-glib/edge-event.h',
+  'gpiod-glib/info-event.h',
+)
+
+gen_enum_c = custom_target('generated-enums-c',
+  input: project_headers + [files('generated-enums.c.template')],
+  output: 'generated-enums.c',
+  command: [
+    glib_mkenums,
+    '--template', '@INPUT3@',
+    '--output', '@OUTPUT@',
+    '@INPUT0@', '@INPUT1@', '@INPUT2@',
+  ],
+)
+
+glib_sources = [
+  'chip.c',
+  'chip-info.c',
+  'edge-event.c',
+  'error.c',
+  'info-event.c',
+  'internal.c',
+  'line-config.c',
+  'line-info.c',
+  'line-request.c',
+  'line-settings.c',
+  'misc.c',
+  'request-config.c',
+]
+
+glib_public_headers = files(
+  'gpiod-glib/chip.h',
+  'gpiod-glib/chip-info.h',
+  'gpiod-glib/edge-event.h',
+  'gpiod-glib/error.h',
+  'gpiod-glib/info-event.h',
+  'gpiod-glib/line.h',
+  'gpiod-glib/line-config.h',
+  'gpiod-glib/line-info.h',
+  'gpiod-glib/line-request.h',
+  'gpiod-glib/line-settings.h',
+  'gpiod-glib/misc.h',
+  'gpiod-glib/request-config.h',
+)
+
+glib_inc = include_directories('.', '../../include')
+
+libgpiod_glib_both = both_libraries('gpiod-glib',
+  sources: glib_sources + [gen_enum_c, gen_enum_h],
+  include_directories: glib_inc,
+  c_args: ['-DG_LOG_DOMAIN="gpiod-glib"', '-DGPIODGLIB_COMPILATION'] + profiling_c_args,
+  link_args: profiling_link_args,
+  dependencies: [libgpiod_dep, glib_dep, gobject_dep, gio_dep, gio_unix_dep],
+  version: libgpiod_glib_version,
+  soversion: libgpiod_glib_soversion,
+  install: true,
+)
+libgpiod_glib_lib = libgpiod_glib_both.get_shared_lib()
+
+install_headers('gpiod-glib.h')
+
+install_headers(glib_public_headers, subdir: 'gpiod-glib')
+
+pkgconfig.generate(libgpiod_glib_lib,
+  name: 'gpiod-glib',
+  description: 'GObject bindings for libgpiod',
+  url: 'https://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git/',
+  version: meson.project_version(),
+  requires_private: ['libgpiod >= 2.1'],
+)
+
+libgpiod_glib_dep = declare_dependency(
+  link_with: libgpiod_glib_lib,
+  include_directories: glib_inc,
+)
+
+if opt_introspection.allowed() and gir_dep.found()
+  gnome = import('gnome')
+
+  glib_gir = gnome.generate_gir(libgpiod_glib_lib,
+    sources: glib_sources + glib_public_headers + [gen_enum_c, gen_enum_h],
+    namespace: 'Gpiodglib',
+    nsversion: '1.0',
+    identifier_prefix: 'Gpiodglib',
+    symbol_prefix: 'gpiodglib',
+    includes: ['Gio-2.0'],
+    header: 'gpiod-glib.h',
+    export_packages: 'gpiod-glib',
+    extra_args: [
+      '--c-include=gpiod-glib.h',
+      '--warn-all',
+      '-DGPIODGLIB_COMPILATION',
+    ],
+    include_directories: glib_inc,
+    install: true,
+  )
+endif
+
+if opt_examples.allowed()
+  subdir('examples')
+endif
+
+if opt_tests.allowed()
+  subdir('tests')
+endif

bindings/cxx/examples/meson.build
@@ -1,0 +1,26 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-FileCopyrightText: 2026 Qualcomm Technologies, Inc. and/or its subsidiaries
+
+cxx_examples = [
+  'async_watch_line_value',
+  'find_line_by_name',
+  'get_chip_info',
+  'get_line_info',
+  'get_line_value',
+  'get_multiple_line_values',
+  'reconfigure_input_to_output',
+  'toggle_line_value',
+  'toggle_multiple_line_values',
+  'watch_line_info',
+  'watch_line_rising',
+  'watch_line_value',
+  'watch_multiple_line_values',
+]
+
+foreach ex : cxx_examples
+  executable(ex,
+    sources: [ex + '.cpp'],
+    include_directories: cxx_inc,
+    dependencies: [libgpiodcxx_dep],
+  )
+endforeach

bindings/cxx/meson.build
@@ -1,0 +1,74 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-FileCopyrightText: 2026 Qualcomm Technologies, Inc. and/or its subsidiaries
+
+cxx_inc = include_directories('.', '../../include')
+
+libgpiodcxx_both = both_libraries('gpiodcxx',
+  sources: [
+    'chip.cpp',
+    'chip-info.cpp',
+    'edge-event-buffer.cpp',
+    'edge-event.cpp',
+    'exception.cpp',
+    'info-event.cpp',
+    'internal.cpp',
+    'line.cpp',
+    'line-config.cpp',
+    'line-info.cpp',
+    'line-request.cpp',
+    'line-settings.cpp',
+    'misc.cpp',
+    'request-builder.cpp',
+    'request-config.cpp',
+  ],
+  include_directories: cxx_inc,
+  cpp_args: ['-fvisibility=hidden'] + profiling_c_args,
+  link_args: profiling_link_args,
+  dependencies: [libgpiod_dep],
+  version: libgpiodcxx_version,
+  soversion: libgpiodcxx_soversion,
+  install: true,
+)
+libgpiodcxx_lib = libgpiodcxx_both.get_shared_lib()
+
+install_headers('gpiod.hpp')
+
+install_headers(
+  'gpiodcxx/chip.hpp',
+  'gpiodcxx/chip-info.hpp',
+  'gpiodcxx/edge-event-buffer.hpp',
+  'gpiodcxx/edge-event.hpp',
+  'gpiodcxx/exception.hpp',
+  'gpiodcxx/info-event.hpp',
+  'gpiodcxx/line.hpp',
+  'gpiodcxx/line-config.hpp',
+  'gpiodcxx/line-info.hpp',
+  'gpiodcxx/line-request.hpp',
+  'gpiodcxx/line-settings.hpp',
+  'gpiodcxx/misc.hpp',
+  'gpiodcxx/request-builder.hpp',
+  'gpiodcxx/request-config.hpp',
+  'gpiodcxx/timestamp.hpp',
+  subdir: 'gpiodcxx',
+)
+
+pkgconfig.generate(libgpiodcxx_lib,
+  name: 'libgpiodcxx',
+  description: 'C++ bindings for libgpiod',
+  url: 'https://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git/',
+  version: meson.project_version(),
+  requires: ['libgpiod'],
+)
+
+libgpiodcxx_dep = declare_dependency(
+  link_with: libgpiodcxx_lib,
+  include_directories: cxx_inc,
+)
+
+if opt_examples.allowed()
+  subdir('examples')
+endif
+
+if opt_tests.allowed()
+  subdir('tests')
+endif

bindings/cxx/tests/meson.build
@@ -1,0 +1,29 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-FileCopyrightText: 2026 Qualcomm Technologies, Inc. and/or its subsidiaries
+
+executable('gpiod-cxx-test',
+  sources: [
+    'check-kernel.cpp',
+    'gpiod-cxx-test-main.cpp',
+    'gpiosim.cpp',
+    'helpers.cpp',
+    'tests-chip.cpp',
+    'tests-chip-info.cpp',
+    'tests-edge-event.cpp',
+    'tests-info-event.cpp',
+    'tests-line.cpp',
+    'tests-line-config.cpp',
+    'tests-line-info.cpp',
+    'tests-line-request.cpp',
+    'tests-line-settings.cpp',
+    'tests-misc.cpp',
+    'tests-request-config.cpp',
+  ],
+  include_directories: [
+    cxx_inc,
+    include_directories('../../..'),
+    include_directories('../../../tests/gpiosim'),
+  ],
+  dependencies: [libgpiodcxx_dep, catch2_test_dep, threads_dep],
+  link_with: [libgpiosim_lib],
+)

bindings/glib/examples/meson.build
@@ -1,0 +1,25 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-FileCopyrightText: 2026 Qualcomm Technologies, Inc. and/or its subsidiaries
+
+glib_examples = [
+  'find_line_by_name_glib',
+  'get_chip_info_glib',
+  'get_line_info_glib',
+  'get_line_value_glib',
+  'get_multiple_line_values_glib',
+  'reconfigure_input_to_output_glib',
+  'toggle_line_value_glib',
+  'toggle_multiple_line_values_glib',
+  'watch_line_info_glib',
+  'watch_line_value_glib',
+  'watch_multiple_edge_rising_glib',
+]
+
+foreach ex : glib_examples
+  executable(ex,
+    sources: [ex + '.c'],
+    include_directories: glib_inc,
+    c_args: ['-DG_LOG_DOMAIN="gpiotools-glib"'],
+    dependencies: [libgpiod_glib_dep, glib_dep, gobject_dep],
+  )
+endforeach

bindings/glib/gpiod-glib/meson.build
@@ -1,0 +1,18 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-FileCopyrightText: 2026 Qualcomm Technologies, Inc. and/or its subsidiaries
+
+gen_enum_h = custom_target('generated-enums-h',
+  input: [
+    files('line.h', 'edge-event.h', 'info-event.h'),
+    files('../generated-enums.h.template'),
+  ],
+  output:  'generated-enums.h',
+  command: [
+    glib_mkenums,
+    '--template', '@INPUT3@',
+    '--output', '@OUTPUT@',
+    '@INPUT0@', '@INPUT1@', '@INPUT2@',
+  ],
+  install: true,
+  install_dir: get_option('includedir') / 'gpiod-glib',
+)

bindings/glib/tests/meson.build
@@ -1,0 +1,27 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-FileCopyrightText: 2026 Qualcomm Technologies, Inc. and/or its subsidiaries
+
+executable('gpiod-glib-test',
+  sources: [
+    'helpers.c',
+    'tests-chip.c',
+    'tests-chip-info.c',
+    'tests-edge-event.c',
+    'tests-info-event.c',
+    'tests-line-config.c',
+    'tests-line-info.c',
+    'tests-line-request.c',
+    'tests-line-settings.c',
+    'tests-misc.c',
+    'tests-request-config.c',
+  ],
+  include_directories: [
+    glib_inc,
+    include_directories('../../..'),
+    include_directories('../../../tests/gpiosim-glib'),
+    include_directories('../../../tests/harness'),
+  ],
+  c_args: ['-DG_LOG_DOMAIN="gpiod-glib-test"'],
+  dependencies: [libgpiod_glib_dep, glib_test_dep, gio_test_dep],
+  link_with: [libgpiosim_lib, libgpiosim_glib_lib, libgpiod_test_harness_lib],
+)

bindings/meson.build
@@ -1,0 +1,18 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-FileCopyrightText: 2026 Qualcomm Technologies, Inc. and/or its subsidiaries
+
+if opt_bindings_cxx.allowed()
+  subdir('cxx')
+endif
+
+if opt_bindings_glib.allowed() and glib_dep.found() and glib_mkenums.found()
+  subdir('glib')
+endif
+
+if opt_bindings_python.allowed()
+  subdir('python')
+endif
+
+if opt_bindings_rust.allowed()
+  subdir('rust')
+endif

commit b2b1267d1ce52fa3b335182360d16b43d72d3e5e

docs: fix build with meson
Migration to meson broke the sphinx build for docs. Wire the gi-docgen
generation into the meson build and fix docs/conf.py to use the new
build system.

Fixes: 97bab4cc87dd ("build: replace autotools with meson & ninja")
Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@oss.qualcomm.com>

bindings/glib/meson.build
@@ -3,6 +3,15 @@
 
 subdir('gpiod-glib')
 
+configure_file(
+  input: 'gi-docgen.toml.in',
+  output: 'gi-docgen.toml',
+  configuration: {
+    'PACKAGE_VERSION': version_str,
+    'PACKAGE_URL': project_url,
+  },
+)
+
 project_headers = files(
   'gpiod-glib/line.h',
   'gpiod-glib/edge-event.h',

docs/conf.py
@@ -18,18 +18,18 @@ author = "Bartosz Golaszewski"
 master_doc = "index"
 source_suffix = ".rst"
 
-# Extract the full version from configure.ac (including -devel, -rc and other
+# Extract the full version from meson.build (including -devel, -rc and other
 # tags).
-with open("../configure.ac", "r") as fd:
+with open("../meson.build", "r") as fd:
     version = ""
     extra = ""
     for line in fd.readlines():
-        match = re.search(r"AC_INIT\(\[libgpiod\], \[(.*?)\]\)", line)
-        if match:
+        match = re.search(r"version:\s*'(.*?)'", line)
+        if match and not version:
             version = match.group(1)
             continue
 
-        match = re.search(r"AC_SUBST\(EXTRA_VERSION, \[(.*?)\]\)", line)
+        match = re.search(r"extra_version\s*=\s*'(.*?)'", line)
         if match:
             extra = match.group(1)
 
@@ -61,6 +61,9 @@ html_theme = "sphinx_rtd_theme" if sphinx_rtd_theme else "default"
 # For some reason on RTD we're in the docs/ directory while during a local
 # build, we're still in the top source directory.
 prefix = "../" if os.getenv("READTHEDOCS") == "True" else "./"
+builddir_prefix = (
+    "../sphinx-builddir" if os.getenv("READTHEDOCS") == "True" else "./sphinx-builddir"
+)
 
 
 def make_glib_docs(app):
@@ -69,8 +72,8 @@ def make_glib_docs(app):
             "gi-docgen",
             "generate",
             "--config",
-            f"{prefix}/bindings/glib/gi-docgen.toml",
-            f"{prefix}/bindings/glib/Gpiodglib-1.0.gir",
+            f"{builddir_prefix}/bindings/glib/gi-docgen.toml",
+            f"{builddir_prefix}/bindings/glib/Gpiodglib-1.0.gir",
             "--output-dir",
             f"{app.outdir}",
         ],
@@ -109,19 +112,20 @@ subprocess.run(["doxygen", "Doxyfile"])
 
 cwd = os.getcwd()
 os.chdir("..")
-subprocess.run(["autoreconf", "-ifv"], check=True)
-subprocess.run(
-    [
-        "./configure",
-        "--enable-tools",
-        "--enable-bindings-glib",
-        "--enable-introspection",
-        "--enable-tools",
-        "--enable-dbus",
-    ],
-    check=True,
-)
-subprocess.run(["make", "-j"], check=True)
+sphinx_builddir = "sphinx-builddir"
+meson_cmd = [
+    "meson",
+    "setup",
+    sphinx_builddir,
+    "-Dtools=enabled",
+    "-Dbindings-glib=enabled",
+    "-Dintrospection=enabled",
+    "-Ddbus=enabled",
+]
+if os.path.exists(sphinx_builddir):
+    meson_cmd.append("--reconfigure")
+subprocess.run(meson_cmd, check=True)
+subprocess.run(["ninja", "-C", sphinx_builddir], check=True)
 os.chdir(cwd)
 
 for page in [
@@ -154,7 +158,7 @@ for page in [
             "--standalone",
             "--wrap=none",
             f"--output={page}.rst",
-            f"../man/{page}.man",
+            f"../sphinx-builddir/man/{page}.1",
         ],
         check=True,
     )