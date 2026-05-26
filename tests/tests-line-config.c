brgl/libgpiod

commit b7ba732e6a93313d4609c4d3ffb426996bf68d02

treewide: libgpiod v2 implementation
This commit is the result of a squash of all the commits created during
the development of libgpiod v2. This is done to preserve bisectability
of the git tree. It contains significant re-writes of the core C library
as well as C++ and python bindings. The tools have been adjusted to work
with the new API but have not undergone significant changes yet.

This commit contains the bulk of the work on libgpiod v2 but the
development continues with more changes planned.

Signed-off-by: Bartosz Golaszewski <brgl@bgdev.pl>
[Ben: fix format specifier for uint64_t]
Signed-off-by: Ben Hutchings <ben.hutchings@mind.be>
[Viresh: Fix ioctl number for gpiod_line_request_reconfigure_lines()]
Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
[Kent: bug fixes, improvements, better pointer contracts, type strictness]
Signed-off-by: Kent Gibson <warthog618@gmail.com>

lib/line-config.c
@@ -1,0 +1,461 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// SPDX-FileCopyrightText: 2022 Bartosz Golaszewski <brgl@bgdev.pl>
+
+#include <errno.h>
+#include <gpiod.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "internal.h"
+
+#define LINES_MAX (GPIO_V2_LINES_MAX)
+
+struct settings_node {
+	struct settings_node *next;
+	struct gpiod_line_settings *settings;
+};
+
+struct per_line_config {
+	unsigned int offset;
+	struct settings_node *node;
+};
+
+struct gpiod_line_config {
+	struct per_line_config line_configs[LINES_MAX];
+	size_t num_configs;
+	struct settings_node *sref_list;
+};
+
+GPIOD_API struct gpiod_line_config *gpiod_line_config_new(void)
+{
+	struct gpiod_line_config *config;
+
+	config = malloc(sizeof(*config));
+	if (!config)
+		return NULL;
+
+	memset(config, 0, sizeof(*config));
+
+	return config;
+}
+
+static void free_refs(struct gpiod_line_config *config)
+{
+	struct settings_node *node, *tmp;
+
+	for (node = config->sref_list; node;) {
+		tmp = node->next;
+		gpiod_line_settings_free(node->settings);
+		free(node);
+		node = tmp;
+	}
+}
+
+GPIOD_API void gpiod_line_config_free(struct gpiod_line_config *config)
+{
+	free_refs(config);
+	free(config);
+}
+
+GPIOD_API void gpiod_line_config_reset(struct gpiod_line_config *config)
+{
+	free_refs(config);
+	memset(config, 0, sizeof(*config));
+}
+
+static struct per_line_config *
+find_config(struct gpiod_line_config *config, unsigned int offset)
+{
+	struct per_line_config *per_line;
+	size_t i;
+
+	for (i = 0; i < config->num_configs; i++) {
+		per_line = &config->line_configs[i];
+
+		if (offset == per_line->offset)
+			return per_line;
+	}
+
+	return &config->line_configs[config->num_configs++];
+}
+
+GPIOD_API int
+gpiod_line_config_add_line_settings(struct gpiod_line_config *config,
+				    const unsigned int *offsets,
+				    size_t num_offsets,
+				    struct gpiod_line_settings *settings)
+{
+	struct per_line_config *per_line;
+	struct settings_node *node;
+	size_t i;
+
+	if ((config->num_configs + num_offsets) > LINES_MAX) {
+		errno = E2BIG;
+		return -1;
+	}
+
+	node = malloc(sizeof(*node));
+	if (!node)
+		return -1;
+
+	if (!settings)
+		node->settings = gpiod_line_settings_new();
+	else
+		node->settings = gpiod_line_settings_copy(settings);
+	if (!node->settings) {
+		free(node);
+		return -1;
+	}
+
+	node->next = config->sref_list;
+	config->sref_list = node;
+
+	for (i = 0; i < num_offsets; i++) {
+		per_line = find_config(config, offsets[i]);
+
+		per_line->offset = offsets[i];
+		per_line->node = node;
+	}
+
+	return 0;
+}
+
+GPIOD_API struct gpiod_line_settings *
+gpiod_line_config_get_line_settings(struct gpiod_line_config *config,
+				    unsigned int offset)
+{
+	struct per_line_config *per_line;
+	size_t i;
+
+	for (i = 0; i < config->num_configs; i++) {
+		per_line = &config->line_configs[i];
+
+		if (per_line->offset == offset)
+			return gpiod_line_settings_copy(
+					per_line->node->settings);
+	}
+
+	errno = ENOENT;
+	return NULL;
+}
+
+GPIOD_API int
+gpiod_line_config_get_offsets(struct gpiod_line_config *config,
+			      size_t *num_offsets,
+			      unsigned int **offsets)
+{
+	unsigned int *offs;
+	size_t i;
+
+	*num_offsets = config->num_configs;
+	*offsets = NULL;
+
+	if (!config->num_configs)
+		return 0;
+
+	offs = calloc(config->num_configs, sizeof(unsigned int));
+	if (!offs)
+		return -1;
+
+	for (i = 0; i < config->num_configs; i++)
+		offs[i] = config->line_configs[i].offset;
+
+	*offsets = offs;
+
+	return 0;
+}
+
+static void set_offsets(struct gpiod_line_config *config,
+			struct gpio_v2_line_request *uapi_cfg)
+{
+	size_t i;
+
+	uapi_cfg->num_lines = config->num_configs;
+
+	for (i = 0; i < config->num_configs; i++)
+		uapi_cfg->offsets[i] = config->line_configs[i].offset;
+}
+
+static bool has_at_least_one_output_direction(struct gpiod_line_config *config)
+{
+	size_t i;
+
+	for (i = 0; i < config->num_configs; i++) {
+		if (gpiod_line_settings_get_direction(
+				config->line_configs[i].node->settings) ==
+		    GPIOD_LINE_DIRECTION_OUTPUT)
+			return true;
+	}
+
+	return false;
+}
+
+static void set_kernel_output_values(uint64_t *mask, uint64_t *vals,
+				     struct gpiod_line_config *config)
+{
+	struct per_line_config *per_line;
+	int value;
+	size_t i;
+
+	gpiod_line_mask_zero(mask);
+	gpiod_line_mask_zero(vals);
+
+	for (i = 0; i < config->num_configs; i++) {
+		per_line = &config->line_configs[i];
+
+		if (gpiod_line_settings_get_direction(
+				per_line->node->settings) !=
+		    GPIOD_LINE_DIRECTION_OUTPUT)
+			continue;
+
+		gpiod_line_mask_set_bit(mask, i);
+		value = gpiod_line_settings_get_output_value(
+						per_line->node->settings);
+		gpiod_line_mask_assign_bit(vals, i,
+				value == GPIOD_LINE_VALUE_ACTIVE ? 1 : 0);
+	}
+}
+
+static void set_output_values(struct gpiod_line_config *config,
+			      struct gpio_v2_line_request *uapi_cfg,
+			      unsigned int *attr_idx)
+{
+	struct gpio_v2_line_config_attribute *attr;
+	uint64_t mask, values;
+
+	if (!has_at_least_one_output_direction(config))
+		return;
+
+	attr = &uapi_cfg->config.attrs[(*attr_idx)++];
+	attr->attr.id = GPIO_V2_LINE_ATTR_ID_OUTPUT_VALUES;
+	set_kernel_output_values(&mask, &values, config);
+	attr->attr.values = values;
+	attr->mask = mask;
+}
+
+static int set_debounce_periods(struct gpiod_line_config *config,
+				struct gpio_v2_line_config *uapi_cfg,
+				unsigned int *attr_idx)
+{
+	struct gpio_v2_line_config_attribute *attr;
+	unsigned long period_i, period_j;
+	uint64_t done, mask;
+	size_t i, j;
+
+	gpiod_line_mask_zero(&done);
+
+	for (i = 0; i < config->num_configs; i++) {
+		if (gpiod_line_mask_test_bit(&done, i))
+			continue;
+
+		gpiod_line_mask_set_bit(&done, i);
+		gpiod_line_mask_zero(&mask);
+
+		period_i = gpiod_line_settings_get_debounce_period_us(
+				config->line_configs[i].node->settings);
+		if (!period_i)
+			continue;
+
+		if (*attr_idx == GPIO_V2_LINE_NUM_ATTRS_MAX) {
+			errno = E2BIG;
+			return -1;
+		}
+
+		attr = &uapi_cfg->attrs[(*attr_idx)++];
+		attr->attr.id = GPIO_V2_LINE_ATTR_ID_DEBOUNCE;
+		attr->attr.debounce_period_us = period_i;
+		gpiod_line_mask_set_bit(&mask, i);
+
+		for (j = i; j < config->num_configs; j++) {
+			period_j = gpiod_line_settings_get_debounce_period_us(
+					config->line_configs[j].node->settings);
+			if (period_i == period_j) {
+				gpiod_line_mask_set_bit(&mask, j);
+				gpiod_line_mask_set_bit(&done, j);
+			}
+		}
+
+		attr->mask = mask;
+	}
+
+	return 0;
+}
+
+static uint64_t make_kernel_flags(struct gpiod_line_settings *settings)
+{
+	uint64_t flags = 0;
+
+	switch (gpiod_line_settings_get_direction(settings)) {
+	case GPIOD_LINE_DIRECTION_INPUT:
+		flags |= GPIO_V2_LINE_FLAG_INPUT;
+		break;
+	case GPIOD_LINE_DIRECTION_OUTPUT:
+		flags |= GPIO_V2_LINE_FLAG_OUTPUT;
+		break;
+	}
+
+	switch (gpiod_line_settings_get_edge_detection(settings)) {
+	case GPIOD_LINE_EDGE_FALLING:
+		flags |= (GPIO_V2_LINE_FLAG_EDGE_FALLING |
+			   GPIO_V2_LINE_FLAG_INPUT);
+		flags &= ~GPIOD_LINE_DIRECTION_OUTPUT;
+		break;
+	case GPIOD_LINE_EDGE_RISING:
+		flags |= (GPIO_V2_LINE_FLAG_EDGE_RISING |
+			   GPIO_V2_LINE_FLAG_INPUT);
+		flags &= ~GPIOD_LINE_DIRECTION_OUTPUT;
+		break;
+	case GPIOD_LINE_EDGE_BOTH:
+		flags |= (GPIO_V2_LINE_FLAG_EDGE_FALLING |
+			   GPIO_V2_LINE_FLAG_EDGE_RISING |
+			   GPIO_V2_LINE_FLAG_INPUT);
+		flags &= ~GPIOD_LINE_DIRECTION_OUTPUT;
+		break;
+	}
+
+	switch (gpiod_line_settings_get_drive(settings)) {
+	case GPIOD_LINE_DRIVE_OPEN_DRAIN:
+		flags |= GPIO_V2_LINE_FLAG_OPEN_DRAIN;
+		break;
+	case GPIOD_LINE_DRIVE_OPEN_SOURCE:
+		flags |= GPIO_V2_LINE_FLAG_OPEN_SOURCE;
+		break;
+	}
+
+	switch (gpiod_line_settings_get_bias(settings)) {
+	case GPIOD_LINE_BIAS_DISABLED:
+		flags |= GPIO_V2_LINE_FLAG_BIAS_DISABLED;
+		break;
+	case GPIOD_LINE_BIAS_PULL_UP:
+		flags |= GPIO_V2_LINE_FLAG_BIAS_PULL_UP;
+		break;
+	case GPIOD_LINE_BIAS_PULL_DOWN:
+		flags |= GPIO_V2_LINE_FLAG_BIAS_PULL_DOWN;
+		break;
+	}
+
+	if (gpiod_line_settings_get_active_low(settings))
+		flags |= GPIO_V2_LINE_FLAG_ACTIVE_LOW;
+
+	switch (gpiod_line_settings_get_event_clock(settings)) {
+	case GPIOD_LINE_EVENT_CLOCK_REALTIME:
+		flags |= GPIO_V2_LINE_FLAG_EVENT_CLOCK_REALTIME;
+		break;
+	case GPIOD_LINE_EVENT_CLOCK_HTE:
+		flags |= GPIO_V2_LINE_FLAG_EVENT_CLOCK_HTE;
+		break;
+	}
+
+	return flags;
+}
+
+static bool settings_equal(struct gpiod_line_settings *left,
+			 struct gpiod_line_settings *right)
+{
+	if (gpiod_line_settings_get_direction(left) !=
+	    gpiod_line_settings_get_direction(right))
+		return false;
+
+	if (gpiod_line_settings_get_edge_detection(left) !=
+	    gpiod_line_settings_get_edge_detection(right))
+		return false;
+
+	if (gpiod_line_settings_get_bias(left) !=
+	    gpiod_line_settings_get_bias(right))
+		return false;
+
+	if (gpiod_line_settings_get_drive(left) !=
+	    gpiod_line_settings_get_drive(right))
+		return false;
+
+	if (gpiod_line_settings_get_active_low(left) !=
+	    gpiod_line_settings_get_active_low(right))
+		return false;
+
+	if (gpiod_line_settings_get_event_clock(left) !=
+	    gpiod_line_settings_get_event_clock(right))
+		return false;
+
+	return true;
+}
+
+static int set_flags(struct gpiod_line_config *config,
+		     struct gpio_v2_line_config *uapi_cfg,
+		     unsigned int *attr_idx)
+{
+	struct gpiod_line_settings *settings_i, *settings_j;
+	struct gpio_v2_line_config_attribute *attr;
+	bool globals_taken = false;
+	uint64_t done, mask;
+	size_t i, j;
+
+	gpiod_line_mask_zero(&done);
+
+	for (i = 0; i < config->num_configs; i++) {
+		if (gpiod_line_mask_test_bit(&done, i))
+			continue;
+
+		gpiod_line_mask_set_bit(&done, i);
+
+		settings_i = config->line_configs[i].node->settings;
+
+		if (!globals_taken) {
+			globals_taken = true;
+			uapi_cfg->flags = make_kernel_flags(settings_i);
+
+			for (j = i; j < config->num_configs; j++) {
+				settings_j =
+					config->line_configs[j].node->settings;
+				if (settings_equal(settings_i, settings_j))
+					gpiod_line_mask_set_bit(&done, j);
+			}
+		} else {
+			gpiod_line_mask_zero(&mask);
+			gpiod_line_mask_set_bit(&mask, i);
+
+			if (*attr_idx == GPIO_V2_LINE_NUM_ATTRS_MAX) {
+				errno = E2BIG;
+				return -1;
+			}
+
+			attr = &uapi_cfg->attrs[(*attr_idx)++];
+			attr->attr.id = GPIO_V2_LINE_ATTR_ID_FLAGS;
+			attr->attr.flags = make_kernel_flags(settings_i);
+
+			for (j = i; j < config->num_configs; j++) {
+				settings_j =
+					config->line_configs[j].node->settings;
+				if (settings_equal(settings_i, settings_j)) {
+					gpiod_line_mask_set_bit(&done, j);
+					gpiod_line_mask_set_bit(&mask, j);
+				}
+			}
+
+			attr->mask = mask;
+		}
+	}
+
+	return 0;
+}
+
+int gpiod_line_config_to_uapi(struct gpiod_line_config *config,
+			      struct gpio_v2_line_request *uapi_cfg)
+{
+	unsigned int attr_idx = 0;
+	int ret;
+
+	set_offsets(config, uapi_cfg);
+	set_output_values(config, uapi_cfg, &attr_idx);
+
+	ret = set_debounce_periods(config, &uapi_cfg->config, &attr_idx);
+	if (ret)
+		return -1;
+
+	ret = set_flags(config, &uapi_cfg->config, &attr_idx);
+	if (ret)
+		return -1;
+
+	uapi_cfg->config.num_attrs = attr_idx;
+
+	return 0;
+}

tests/tests-line-config.c
@@ -1,0 +1,216 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-FileCopyrightText: 2017-2021 Bartosz Golaszewski <bartekgola@gmail.com>
+
+#include <errno.h>
+#include <glib.h>
+#include <gpiod.h>
+
+#include "gpiod-test.h"
+#include "gpiod-test-helpers.h"
+#include "gpiod-test-sim.h"
+
+#define GPIOD_TEST_GROUP "line-config"
+
+GPIOD_TEST_CASE(too_many_lines)
+{
+	g_autoptr(struct_gpiod_line_settings) settings = NULL;
+	g_autoptr(struct_gpiod_line_config) config = NULL;
+	guint offsets[65], i;
+	gint ret;
+
+	settings = gpiod_test_create_line_settings_or_fail();
+	config = gpiod_test_create_line_config_or_fail();
+
+	for (i = 0; i < 65; i++)
+		offsets[i] = i;
+
+	ret = gpiod_line_config_add_line_settings(config, offsets, 65,
+						  settings);
+	g_assert_cmpint(ret, <, 0);
+	g_assert_cmpint(errno, ==, E2BIG);
+}
+
+GPIOD_TEST_CASE(get_line_settings)
+{
+	static const guint offsets[] = { 0, 1, 2, 3 };
+
+	g_autoptr(struct_gpiod_line_settings) settings = NULL;
+	g_autoptr(struct_gpiod_line_settings) retrieved = NULL;
+	g_autoptr(struct_gpiod_line_config) config = NULL;
+
+	settings = gpiod_test_create_line_settings_or_fail();
+	config = gpiod_test_create_line_config_or_fail();
+
+	gpiod_line_settings_set_direction(settings, GPIOD_LINE_DIRECTION_INPUT);
+	gpiod_line_settings_set_bias(settings, GPIOD_LINE_BIAS_PULL_DOWN);
+	gpiod_test_line_config_add_line_settings_or_fail(config, offsets, 4,
+							 settings);
+
+	retrieved = gpiod_line_config_get_line_settings(config, 2);
+	g_assert_nonnull(retrieved);
+	gpiod_test_return_if_failed();
+
+	g_assert_cmpint(gpiod_line_settings_get_direction(settings), ==,
+			GPIOD_LINE_DIRECTION_INPUT);
+	g_assert_cmpint(gpiod_line_settings_get_bias(settings), ==,
+			GPIOD_LINE_BIAS_PULL_DOWN);
+}
+
+GPIOD_TEST_CASE(too_many_attrs)
+{
+	g_autoptr(GPIOSimChip) sim = g_gpiosim_chip_new("num-lines", 8, NULL);
+	g_autoptr(struct_gpiod_chip) chip = NULL;
+	g_autoptr(struct_gpiod_line_config) line_cfg = NULL;
+	g_autoptr(struct_gpiod_line_settings) settings = NULL;
+	g_autoptr(struct_gpiod_line_request) request = NULL;
+	guint offset;
+
+	chip = gpiod_test_open_chip_or_fail(g_gpiosim_chip_get_dev_path(sim));
+	settings = gpiod_test_create_line_settings_or_fail();
+	line_cfg = gpiod_test_create_line_config_or_fail();
+
+	gpiod_line_settings_set_direction(settings,
+					  GPIOD_LINE_DIRECTION_OUTPUT);
+	gpiod_line_settings_set_output_value(settings, GPIOD_LINE_VALUE_ACTIVE);
+	offset = 0;
+	gpiod_test_line_config_add_line_settings_or_fail(line_cfg, &offset, 1,
+							 settings);
+
+	gpiod_line_settings_set_direction(settings, GPIOD_LINE_DIRECTION_INPUT);
+	gpiod_line_settings_set_debounce_period_us(settings, 1000);
+	gpiod_line_settings_set_edge_detection(settings, GPIOD_LINE_EDGE_BOTH);
+	offset = 1;
+	gpiod_test_line_config_add_line_settings_or_fail(line_cfg, &offset, 1,
+							 settings);
+
+	gpiod_line_settings_set_bias(settings, GPIOD_LINE_BIAS_PULL_UP);
+	offset = 2;
+	gpiod_test_line_config_add_line_settings_or_fail(line_cfg, &offset, 1,
+							 settings);
+
+	gpiod_line_settings_set_bias(settings, GPIOD_LINE_BIAS_PULL_DOWN);
+	offset = 3;
+	gpiod_test_line_config_add_line_settings_or_fail(line_cfg, &offset, 1,
+							 settings);
+
+	gpiod_line_settings_set_bias(settings, GPIOD_LINE_BIAS_DISABLED);
+	offset = 4;
+	gpiod_test_line_config_add_line_settings_or_fail(line_cfg, &offset, 1,
+							 settings);
+
+	gpiod_line_settings_set_active_low(settings, true);
+	offset = 5;
+	gpiod_test_line_config_add_line_settings_or_fail(line_cfg, &offset, 1,
+							 settings);
+
+	gpiod_line_settings_set_edge_detection(settings,
+					       GPIOD_LINE_EDGE_FALLING);
+	offset = 6;
+	gpiod_test_line_config_add_line_settings_or_fail(line_cfg, &offset, 1,
+							 settings);
+
+	gpiod_line_settings_set_event_clock(settings,
+					     GPIOD_LINE_EVENT_CLOCK_REALTIME);
+	offset = 7;
+	gpiod_test_line_config_add_line_settings_or_fail(line_cfg, &offset, 1,
+							 settings);
+
+	gpiod_line_settings_reset(settings);
+
+	gpiod_line_settings_set_direction(settings,
+					  GPIOD_LINE_DIRECTION_OUTPUT);
+	gpiod_line_settings_set_drive(settings, GPIOD_LINE_DRIVE_OPEN_DRAIN);
+	offset = 8;
+	gpiod_test_line_config_add_line_settings_or_fail(line_cfg, &offset, 1,
+							 settings);
+
+	gpiod_line_settings_set_drive(settings, GPIOD_LINE_DRIVE_OPEN_SOURCE);
+	offset = 9;
+	gpiod_test_line_config_add_line_settings_or_fail(line_cfg, &offset, 1,
+							 settings);
+
+	request = gpiod_chip_request_lines(chip, NULL, line_cfg);
+	g_assert_null(request);
+	g_assert_cmpint(errno, ==, E2BIG);
+}
+
+GPIOD_TEST_CASE(reset_config)
+{
+	static const guint offsets[] = { 0, 1, 2, 3 };
+
+	g_autoptr(struct_gpiod_line_settings) settings = NULL;
+	g_autoptr(struct_gpiod_line_settings) retrieved0 = NULL;
+	g_autoptr(struct_gpiod_line_settings) retrieved1 = NULL;
+	g_autoptr(struct_gpiod_line_config) config = NULL;
+
+	settings = gpiod_test_create_line_settings_or_fail();
+	config = gpiod_test_create_line_config_or_fail();
+
+	gpiod_line_settings_set_direction(settings, GPIOD_LINE_DIRECTION_INPUT);
+	gpiod_line_settings_set_bias(settings, GPIOD_LINE_BIAS_PULL_DOWN);
+	gpiod_test_line_config_add_line_settings_or_fail(config, offsets, 4,
+							 settings);
+
+	retrieved0 = gpiod_line_config_get_line_settings(config, 2);
+	g_assert_nonnull(retrieved0);
+	gpiod_test_return_if_failed();
+
+	g_assert_cmpint(gpiod_line_settings_get_direction(settings), ==,
+			GPIOD_LINE_DIRECTION_INPUT);
+	g_assert_cmpint(gpiod_line_settings_get_bias(settings), ==,
+			GPIOD_LINE_BIAS_PULL_DOWN);
+
+	gpiod_line_config_reset(config);
+
+	retrieved1 = gpiod_line_config_get_line_settings(config, 2);
+	g_assert_null(retrieved1);
+}
+
+GPIOD_TEST_CASE(get_offsets)
+{
+	g_autoptr(struct_gpiod_line_settings) settings = NULL;
+	g_autoptr(struct_gpiod_line_config) config = NULL;
+	g_autofree guint *config_offs = NULL;
+	guint offsets[8];
+	size_t num_offsets;
+	gint ret;
+
+	settings = gpiod_test_create_line_settings_or_fail();
+	config = gpiod_test_create_line_config_or_fail();
+
+	gpiod_line_settings_set_direction(settings, GPIOD_LINE_DIRECTION_INPUT);
+	gpiod_line_settings_set_bias(settings, GPIOD_LINE_BIAS_PULL_DOWN);
+	offsets[0] = 2;
+	offsets[1] = 4;
+	gpiod_test_line_config_add_line_settings_or_fail(config, offsets, 2,
+							 settings);
+
+	gpiod_line_settings_set_edge_detection(settings, GPIOD_LINE_EDGE_BOTH);
+	offsets[0] = 6;
+	offsets[1] = 7;
+	gpiod_test_line_config_add_line_settings_or_fail(config, offsets, 2,
+							 settings);
+
+	ret = gpiod_line_config_get_offsets(config, &num_offsets, &config_offs);
+	g_assert_cmpint(ret, ==, 0);
+	g_assert_cmpuint(num_offsets, ==, 4);
+	g_assert_cmpuint(config_offs[0], ==, 2);
+	g_assert_cmpuint(config_offs[1], ==, 4);
+	g_assert_cmpuint(config_offs[2], ==, 6);
+	g_assert_cmpuint(config_offs[3], ==, 7);
+}
+
+GPIOD_TEST_CASE(get_0_offsets)
+{
+	g_autoptr(struct_gpiod_line_config) config = NULL;
+	g_autofree guint *offsets = NULL;
+	size_t num_offsets;
+	gint ret;
+
+	config = gpiod_test_create_line_config_or_fail();
+
+	ret = gpiod_line_config_get_offsets(config, &num_offsets, &offsets);
+	g_assert_cmpint(ret, ==, 0);
+	g_assert_cmpuint(num_offsets, ==, 0);
+	g_assert_null(offsets);
+}

bindings/python/gpiod/ext/line-config.c
@@ -1,0 +1,133 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// SPDX-FileCopyrightText: 2022 Bartosz Golaszewski <brgl@bgdev.pl>
+
+#include "internal.h"
+
+typedef struct {
+	PyObject_HEAD;
+	struct gpiod_line_config *cfg;
+} line_config_object;
+
+static int line_config_init(line_config_object *self,
+		       PyObject *Py_UNUSED(args), PyObject *Py_UNUSED(ignored))
+{
+	self->cfg = gpiod_line_config_new();
+	if (!self->cfg) {
+		Py_gpiod_SetErrFromErrno();
+		return -1;
+	}
+
+	return 0;
+}
+
+static void line_config_finalize(line_config_object *self)
+{
+	if (self->cfg)
+		gpiod_line_config_free(self->cfg);
+}
+
+static unsigned int *make_offsets(PyObject *obj, Py_ssize_t len)
+{
+	unsigned int *offsets;
+	PyObject *offset;
+	Py_ssize_t i;
+
+	offsets = PyMem_Calloc(len, sizeof(unsigned int));
+	if (!offsets)
+		return (unsigned int *)PyErr_NoMemory();
+
+	for (i = 0; i < len; i++) {
+		offset = PyList_GetItem(obj, i);
+		if (!offset) {
+			PyMem_Free(offsets);
+			return NULL;
+		}
+
+		offsets[i] = Py_gpiod_PyLongAsUnsignedInt(offset);
+		if (PyErr_Occurred()) {
+			PyMem_Free(offsets);
+			return NULL;
+		}
+	}
+
+	return offsets;
+}
+
+static PyObject *
+line_config_add_line_settings(line_config_object *self, PyObject *args)
+{
+	PyObject *offsets_obj, *settings_obj;
+	struct gpiod_line_settings *settings;
+	unsigned int *offsets;
+	Py_ssize_t num_offsets;
+	int ret;
+
+	ret = PyArg_ParseTuple(args, "OO", &offsets_obj, &settings_obj);
+	if (!ret)
+		return NULL;
+
+	num_offsets = PyObject_Size(offsets_obj);
+	if (num_offsets < 0)
+		return NULL;
+
+	offsets = make_offsets(offsets_obj, num_offsets);
+	if (!offsets)
+		return NULL;
+
+	settings = Py_gpiod_LineSettingsGetData(settings_obj);
+	if (!settings) {
+		PyMem_Free(offsets);
+		return NULL;
+	}
+
+	ret = gpiod_line_config_add_line_settings(self->cfg, offsets,
+						  num_offsets, settings);
+	PyMem_Free(offsets);
+	if (ret)
+		return Py_gpiod_SetErrFromErrno();
+
+	Py_RETURN_NONE;
+}
+
+static PyMethodDef line_config_methods[] = {
+	{
+		.ml_name = "add_line_settings",
+		.ml_meth = (PyCFunction)line_config_add_line_settings,
+		.ml_flags = METH_VARARGS,
+	},
+	{ }
+};
+
+PyTypeObject line_config_type = {
+	PyVarObject_HEAD_INIT(NULL, 0)
+	.tp_name = "gpiod._ext.LineConfig",
+	.tp_basicsize = sizeof(line_config_object),
+	.tp_flags = Py_TPFLAGS_DEFAULT,
+	.tp_new = PyType_GenericNew,
+	.tp_init = (initproc)line_config_init,
+	.tp_finalize = (destructor)line_config_finalize,
+	.tp_dealloc = (destructor)Py_gpiod_dealloc,
+	.tp_methods = line_config_methods,
+};
+
+struct gpiod_line_config *Py_gpiod_LineConfigGetData(PyObject *obj)
+{
+	line_config_object *line_cfg;
+	PyObject *type;
+
+	type = PyObject_Type(obj);
+	if (!type)
+		return NULL;
+
+	if ((PyTypeObject *)type != &line_config_type) {
+		PyErr_SetString(PyExc_TypeError,
+				"not a gpiod._ext.LineConfig object");
+		Py_DECREF(type);
+		return NULL;
+	}
+	Py_DECREF(type);
+
+	line_cfg = (line_config_object *)obj;
+
+	return line_cfg->cfg;
+}

commit ec46215f7b8f4481e6c68ae3c34d55cc77b56d40

core: allow modifying settings for existing offsets in a full line-config
gpiod_line_config_add_line_settings() checks the remaining capacity
using the raw num_offsets from the caller. If the config is already at
the 64-line limit, any call - even one that would only update settings
for already configured offsets - would fail with E2BIG making it
impossible to reconfigure a line once the config is full.

Extend gpiod_line_config_add_line_settings() to only count offsets that
would require a new config slot and use this number to check if the
line-config object still has capacity.

Add a test case allowing to verify the fix.

Fixes: b7ba732e6a93 ("treewide: libgpiod v2 implementation")
Link: https://patch.msgid.link/20260522-line-config-full-config-bug-v1-1-b138958bf05d@oss.qualcomm.com
Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@oss.qualcomm.com>

lib/line-config.c
@@ -90,13 +90,50 @@ static struct per_line_config *find_config(struct gpiod_line_config *config,
 	return &config->line_configs[config->num_configs++];
 }
 
+/*
+ * Count offsets in the input array that would require a new slot in the
+ * config: those not already present in config->line_configs and not repeated
+ * earlier in the same offsets array.
+ */
+static size_t count_new_offsets(const struct gpiod_line_config *config,
+				const unsigned int *offsets, size_t num_offsets)
+{
+	size_t i, j, num_new = 0;
+	bool existing;
+
+	for (i = 0; i < num_offsets; i++) {
+		existing = false;
+
+		for (j = 0; j < config->num_configs; j++) {
+			if (config->line_configs[j].offset == offsets[i]) {
+				existing = true;
+				break;
+			}
+		}
+
+		if (!existing) {
+			for (j = 0; j < i; j++) {
+				if (offsets[j] == offsets[i]) {
+					existing = true;
+					break;
+				}
+			}
+		}
+
+		if (!existing)
+			num_new++;
+	}
+
+	return num_new;
+}
+
 GPIOD_API int gpiod_line_config_add_line_settings(
 	struct gpiod_line_config *config, const unsigned int *offsets,
 	size_t num_offsets, struct gpiod_line_settings *settings)
 {
 	struct per_line_config *per_line;
 	struct settings_node *node, *old;
-	size_t i;
+	size_t i, new_offsets;
 
 	assert(config);
 
@@ -105,7 +142,9 @@ GPIOD_API int gpiod_line_config_add_line_settings(
 		return -1;
 	}
 
-	if ((config->num_configs + num_offsets) > LINES_MAX) {
+	new_offsets = count_new_offsets(config, offsets, num_offsets);
+
+	if ((config->num_configs + new_offsets) > LINES_MAX) {
 		errno = E2BIG;
 		return -1;
 	}

tests/tests-line-config.c
@@ -512,3 +512,28 @@ GPIOD_TEST_CASE(dont_allow_line_config_to_balloon_out_of_control)
 	g_test_trap_subprocess(NULL, 0, G_TEST_SUBPROCESS_DEFAULT);
 	g_test_trap_assert_passed();
 }
+
+GPIOD_TEST_CASE(update_existing_offset_in_full_config)
+{
+	g_autoptr(struct_gpiod_line_settings) settings = NULL;
+	g_autoptr(struct_gpiod_line_config) config = NULL;
+	guint offsets[64], i;
+	gint ret;
+
+	settings = gpiod_test_create_line_settings_or_fail();
+	config = gpiod_test_create_line_config_or_fail();
+
+	for (i = 0; i < 64; i++)
+		offsets[i] = i;
+
+	/* Fill the config to capacity with all 64 offsets. */
+	gpiod_test_line_config_add_line_settings_or_fail(config, offsets, 64,
+							 settings);
+
+	g_assert_cmpuint(gpiod_line_config_get_num_configured_offsets(config),
+			 ==, 64);
+
+	gpiod_line_settings_set_direction(settings, GPIOD_LINE_DIRECTION_INPUT);
+	ret = gpiod_line_config_add_line_settings(config, offsets, 1, settings);
+	g_assert_cmpint(ret, ==, 0);
+}